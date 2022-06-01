4961c795ca840d54f57972d2ce158d010472b7e8
JMG001 <91408617+JMG001@users.noreply.github.com>
2022-06-01 15:25:40 +1000

    Update CreateStack.sh

--- 2_Automation/CreateStack.sh
+++ 2_Automation/CreateStack.sh
@@ -1,1 +1,4 @@
-
+aws cloudformation create-stack \
+    --stack-name SimpleApp 
+    --template-body file://sampletemplate.json 
+    --parameters ParameterKey=KeyPairName,ParameterValue=TestKey ParameterKey=SubnetIDs,ParameterValue=SubnetID1\\,SubnetID2
