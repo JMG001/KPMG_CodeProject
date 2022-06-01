6996a24061d35e43ffc5eea31accad7180714cbc
JMG001 <91408617+JMG001@users.noreply.github.com>
2022-06-01 15:36:31 +1000

    Update CreateStack.sh

--- 2_Automation/CreateStack.sh
+++ 2_Automation/CreateStack.sh
@@ -1,10 +1,10 @@
-aws cloudformation create-stack
-    --stack-name SimpleAppStack 
-    --template-body file://BuildInfra.json 
-    --parameters ParameterKey=KeyName,ParameterValue=MyKeyPair ParameterKey=WebInstanceType,ParameterValue=t2.small Parameterkey=SSHLocation,ParameterValue=0.0.0.0/0 ParameterKey=DBUser,ParameterValue=DBAdminUser ParameterKey=DBPassword,ParameterValue=DBAdminPassword ParameterKey=WebAppRG,ParameterValue=WebAppResGrp
+aws cloudformation create-stack \
+    --stack-name SimpleAppStack \
+    --template-body file://BuildInfra.json \
+    --parameters ParameterKey=KeyName,ParameterValue=MyKeyPair ParameterKey=WebInstanceType,ParameterValue=t2.small Parameterkey=SSHLocation,ParameterValue=0.0.0.0/0 ParameterKey=DBUser,ParameterValue=DBAdminUser ParameterKey=DBPassword,ParameterValue=DBAdminPassword ParameterKey=WebAppRG,ParameterValue=WebAppResGrp \
 
 if ! [ "$?" = "0" ]; then
-	exitWithErrorMessage "Cannot create stack ${stackName}!"
+	exitWithErrorMessage "Cannot create stack SimpleAppStack!"
 fi
 
 #Wait for completion
