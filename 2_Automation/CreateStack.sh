23a1911c3a19fb7fde9df7bc3399fe05b0698f13
JMG001 <91408617+JMG001@users.noreply.github.com>
2022-06-01 15:32:12 +1000

    Update CreateStack.sh

--- 2_Automation/CreateStack.sh
+++ 2_Automation/CreateStack.sh
@@ -1,4 +1,11 @@
 aws cloudformation create-stack
-    --stack-name SimpleApp 
+    --stack-name SimpleAppStack 
     --template-body file://BuildInfra.json 
-    --parameters ParameterKey=KeyPairName,ParameterValue=TestKey ParameterKey=SubnetIDs,ParameterValue=SubnetID1\\,SubnetID2
+    --parameters ParameterKey=KeyName,ParameterValue=MyKeyPair ParameterKey=WebInstanceType,ParameterValue=t2.small Parameterkey=SSHLocation,ParameterValue=0.0.0.0/0 ParameterKey=DBUser,ParameterValue=DBAdminUser ParameterKey=DBPassword,ParameterValue=DBAdminPassword ParameterKey=WebAppRG,ParameterValue=WebAppResGrp
+
+if ! [ "$?" = "0" ]; then
+	exitWithErrorMessage "Cannot create stack ${stackName}!"
+fi
+
+#Wait for completion
+waitForState SimpleAppStack "CREATE_COMPLETE"
